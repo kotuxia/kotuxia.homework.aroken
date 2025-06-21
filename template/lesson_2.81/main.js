ec25c4ba10347e538ec7b4dff1283b4e282007dd
Kate <167463085+kotuxia@users.noreply.github.com>
2025-06-21 12:31:11 +0300

    Урок 2.81

---
+++ template/lesson_2.81/main.js
@@ -0,0 +1,13 @@
+// const array = [4, 6, 7, 1, 0, 2, 5, 7, 24, 13]
+
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 == 0) {
+//         console.log(array[i])
+//     }
+// }
+
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+for (let j = rainbow.length - 1; j >= 0; j--) {
+    console.log(rainbow[j])
+}
