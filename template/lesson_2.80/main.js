91ef791d60ccfb656982717fa4d764fe5bb64854
Kate <167463085+kotuxia@users.noreply.github.com>
2025-06-21 12:30:42 +0300

    Урок 2.80

---
+++ template/lesson_2.80/main.js
@@ -0,0 +1,24 @@
+// for (let i = 2; i <= 12; i += 2) {
+//     console.log(i)
+// }
+
+
+// for (j = 10; j >= 1; j--) {
+//     console.log(j)
+// }
+
+
+// let i = 2
+
+// while (i <= 12) {
+//     console.log(i)
+//     i += 2
+// }
+
+
+// let j = 10
+
+// while (j >= 1) {
+//     console.log(j)
+//     j--
+// }
