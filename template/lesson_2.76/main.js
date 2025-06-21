fe9d5b10e69aa8974b063545b0a1c5a65abee7b0
Kate <167463085+kotuxia@users.noreply.github.com>
2025-06-21 12:29:41 +0300

    Урок 2.76

---
+++ template/lesson_2.76/main.js
@@ -0,0 +1,16 @@
+// 1. let и const - 2 метода объявления переменной. С помощью let мы задаем переменную, значение которой можно поменять и переопределить.
+// С помощью const мы задаем переменную, которая является константой. Ее нельзя переопределить и задать новое значение.
+
+// 2.
+
+const firstName = 'Kate'
+const lastName = 'Kulyak'
+let age = 20
+const myCatName = 'Musya'
+let mood = 'Having fun'
+
+console.log(firstName)
+console.log(lastName)
+console.log(age)
+console.log(myCatName)
+console.log(mood)
